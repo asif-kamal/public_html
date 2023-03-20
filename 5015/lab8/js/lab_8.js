6e38bbf102672b63733ed6cf02e38e84d3308c92
Asif Kamal <tua90776@CIS-Linux2.temple.edu>
2023-03-20 17:20:32 -0400

    Modified to run6_1_state and run6_2_state

--- 5015/lab8/js/lab_8.js
+++ 5015/lab8/js/lab_8.js
@@ -1,6 +1,6 @@
 var db_state = 0;
-var run6_1 = 0;
-var run6_2 = 0;
+var run6_1_state = 0;
+var run6_2_state = 0;
 var report_state = 0;
 
 $(document).ready(function() {
@@ -20,23 +20,23 @@ $(document).ready(function() {
         };
     });
     $("#run6_1").click(function() {
-        if (run6_1 == 0) { //Hide all objects
-            run6_1 = 1;
+        if (run6_1_state == 0) { //Hide all objects
+            run6_1_state = 1;
             $("#Lab6_1").hide();
             $("#run6_1").html("<a class='nav-link active' href='#'><i class='fas fa-stop'></i> Show Lab 6.1</a>");
         } else {
-            run6_1 = 0;
+            run6_1_state = 0;
             $("#Lab6_1").show();
             $("#run6_1").html("<a class='nav-link active' href='#'><i class='fas fa-square-check'></i> Hide Lab 6.1</a>");
         };
     });
     $("#run6_2").click(function() {
-        if (run6_2 == 0) { //Hide all objects
-            run6_2 = 1;
+        if (run6_2_state == 0) { //Hide all objects
+            run6_2_state = 1;
             $("#Lab6_2").hide();
             $("#run6_2").html("<a class='nav-link active' href='#'><i class='fas fa-stop'></i> Show Lab 6.2</a>");
         } else {
-            run6_1 = 0;
+            run6_2_state = 0;
             $("#Lab6_2").show();
             $("#run6_2").html("<a class='nav-link active' href='#'><i class='fas fa-square-check'></i> Hide Lab 6.2</a>");
         };
