7bc738f58136af13ebfc65a57eb9cc039fc75de0
Asif Kamal <tua90776@CIS-Linux2.temple.edu>
2023-03-20 17:06:50 -0400

    Built show hide function for db, run6_1, run6_2

--- 5015/lab8/js/lab_8.js
+++ 5015/lab8/js/lab_8.js
@@ -5,13 +5,40 @@ var report_state = 0;
 
 $(document).ready(function() {
     $("#showAll").click(function() {
-        if (db_state == 0) {
+        if (db_state == 0) { //Hide all objects
             db_state = 1;
-            $("#showAll").html("<i class='fa-solid fa-plus'></i> Show Dashboard");
+            $("#Lab6_2").hide();
+            $("#Lab6_1").hide();
+            $("#Report").hide();
+            $("#showAll").html("<a class='nav-link active' href='#'><i class='fa-solid fa-plus'></i> Show Dashboard</a>");
         } else {
             db_state = 0;
-            $("#showAll").html("<i class='fa-solid fa-minus'></i> Hide Dashboard");
+            $("#Lab6_2").show();
+            $("#Lab6_1").show();
+            $("#Report").show();
+            $("#showAll").html("<a class='nav-link active' href='#'><i class='fa-solid fa-minus'></i> Hide Dashboard</a>");
+        };
+    });
+    $("#run6_1").click(function() {
+        if (run6_1 == 0) { //Hide all objects
+            run6_1 = 1;
+            $("#Lab6_1").hide();
+            $("#run6_1").html("<a class='nav-link active' href='#'><i class='fas fa-stop'></i> Show Lab 6.1</a>");
+        } else {
+            run6_1 = 0;
+            $("#Lab6_1").show();
+            $("#run6_1").html("<a class='nav-link active' href='#'><i class='fas fa-square-check'></i> Hide Lab 6.1</a>");
+        };
+    });
+    $("#run6_2").click(function() {
+        if (run6_2 == 0) { //Hide all objects
+            run6_2 = 1;
+            $("#Lab6_2").hide();
+            $("#run6_2").html("<a class='nav-link active' href='#'><i class='fas fa-stop'></i> Show Lab 6.2</a>");
+        } else {
+            run6_1 = 0;
+            $("#Lab6_2").show();
+            $("#run6_2").html("<a class='nav-link active' href='#'><i class='fas fa-square-check'></i> Hide Lab 6.2</a>");
         };
     });
 });
-
