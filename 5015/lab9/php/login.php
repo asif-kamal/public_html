7887e0e130568f5c1e1766cf52a0ff51c5bd9175
Asif Kamal <tua90776@CIS-Linux2.temple.edu>
2023-04-03 17:08:38 -0400

    commented out header

--- 5015/lab9/php/login.php
+++ 5015/lab9/php/login.php
@@ -2,9 +2,9 @@
     session_start();
     $Email = $_POST["inputEmail"];
     $Password = md5($_POST["inputPassword"]);
-    // print "Email = ($Email) Password = ($Password) </br>";
+    print "Email = ($Email) Password = ($Password) </br>";
     $RememberMe = $_POST["rememberMe"];
-    // print "rememberMe = ($RememberMe) </br>";
-    header("Location: ../dashboard.php");
+    print "rememberMe = ($RememberMe) </br>";
+    // header("Location: ../dashboard.php");
     exit();
 ?>
